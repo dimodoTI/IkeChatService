687188fba358b6c33ea410a4d4fc25c251b78ee3
BrunoMon <brunomon@hotmail.com>
2020-08-08 13:05:12 -0300

    Arreglo tipo de dato Leido respondido

---
+++ Migrations/20200808160235_volverCambioLeidoRespondido.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ChatApi.Migrations
+{
+    public partial class volverCambioLeidoRespondido : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Respondido",
+                table: "Chat",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Leido",
+                table: "Chat",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Respondido",
+                table: "Chat",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Leido",
+                table: "Chat",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+    }
+}
