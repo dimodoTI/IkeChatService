f2d0b1babd526fc1ca8053ee56781de4c7240c23
BrunoMon <brunomon@hotmail.com>
2020-08-13 17:44:30 -0300

    Sacar campo esPresgunta y agregar tipo

---
+++ Migrations/20200813202600_campoTipo.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ChatApi.Migrations
+{
+    public partial class campoTipo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EsPregunta",
+                table: "Chat");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Tipo",
+                table: "Chat",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tipo",
+                table: "Chat");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "EsPregunta",
+                table: "Chat",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
